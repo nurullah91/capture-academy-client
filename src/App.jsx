6c7dd3c1cf8628f1c334ba6f2caa8620a84260c3
nurullah91 <mdnurullah.ptk@gmail.com>
2023-06-07 11:08:12 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from './pages/Shared/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <div>
+      <NavBar></NavBar>
+      
+    </div>
+  )
+}
+
+export default App
