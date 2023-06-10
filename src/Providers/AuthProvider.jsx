2c012881570754fac9226d6bc32c9ac92fe74a1e
nurullah91 <mdnurullah.ptk@gmail.com>
2023-06-10 09:29:52 +0600

    firebase setup and sign in, signUp, login added

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,59 @@
+import { createContext, useState } from 'react';
+import { app } from '../firebase/firebase.config';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { useEffect } from 'react';
+const AuthContext = createContext(null);
+const auth = getAuth(app)
+const AuthProvider = () => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    // create a new user through firebase
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // login with google
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider)
+
+    }
+
+    // get user and user info
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+
+            // if(currentUser){
+            //     axios.post()
+            // }
+        })
+
+        return () => {
+            unSubscribe()
+        }
+    }, [])
+
+    const authInfo = {
+        loading,
+        user,
+        createUser,
+        login,
+        googleSignIn
+
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}></AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
