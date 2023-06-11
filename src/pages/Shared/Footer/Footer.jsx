6847605d061d86f2feff1d545a018aaa8984d20d
nurullah91 <mdnurullah.ptk@gmail.com>
2023-06-12 01:14:49 +0600

    footer modified

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,21 +1,36 @@
 import { Link } from "react-router-dom";
-import { FaFacebook } from 'react-icons/fa';
+import { FaEnvelope, FaFacebook, FaLinkedin, FaLocationArrow, FaPhone, FaSearchLocation, FaTwitter } from 'react-icons/fa';
+import { FcGoogle } from "react-icons/fc";
 
 const Footer = () => {
     return (
-        <div>
-            <footer className="footer p-10 bg-neutral text-neutral-content">
+        <div className="bg-neutral text-neutral-content">
+            <footer className="footer p-10 ">
                 <div>
-                    <img src="https://i.ibb.co/gWk7nBN/Black-Yellow-White-Modern-Studio-Photo-Logo-1-removebg-preview-1.png" alt="" />
-                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                    <img className="h-24 w-24" src="https://i.ibb.co/gWk7nBN/Black-Yellow-White-Modern-Studio-Photo-Logo-1-removebg-preview-1.png" alt="" />
+                    <p className="font-bold">Capture Academy<br />Teaching reliable Skills On Photography since 2023</p>
                 </div>
+                    <div>
+                        <span className="footer-title">Contact Us</span>
+                        <p className="flex items-center"><FaEnvelope className="mr-3"></FaEnvelope> captureacademy@sample.com</p>
+
+                        <p className="flex items-center"><FaPhone className="mr-3"></FaPhone> 0179668991</p>
+                        <p className="flex items-center"><FaSearchLocation className="mr-3"></FaSearchLocation> Barishal, Bangladesh</p>
+
+                    </div>
                 <div>
                     <span className="footer-title">Social</span>
-                    <div className="grid grid-flow-col gap-4">
+                    <div className="grid grid-cols-4 md:grid-cols-3 lg:grid-cols-4 gap-4">
                         <span className="text-3xl"><Link><FaFacebook></FaFacebook></Link></span>
+                        <span className="text-3xl"><Link><FaTwitter></FaTwitter></Link></span>
+                        <span className="text-3xl"><Link><FaLinkedin></FaLinkedin></Link></span>
+                        <span className="text-3xl"><Link><FcGoogle></FcGoogle></Link></span>
                     </div>
                 </div>
+                                
+                                 
             </footer>
+            <h2 className="text-center pb-10 pt-5 border-t-2">All rights deserve Capture Academy @2023</h2>
         </div>
     );
 };
