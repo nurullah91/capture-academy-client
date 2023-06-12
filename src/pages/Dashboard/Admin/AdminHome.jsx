c766dec45a2ab55748fb752855ff64ff8f843ab3
nurullah91 <mdnurullah.ptk@gmail.com>
2023-06-12 12:24:39 +0600

    dashboard homepage animatoin added

--- src/pages/Dashboard/Admin/AdminHome.jsx
+++ src/pages/Dashboard/Admin/AdminHome.jsx
@@ -1,11 +1,15 @@
 import useAuth from "../../../Hooks/useAuth";
-
+import welcome from '../../../../public/welcome.json';
+import Lottie from "lottie-react";
 const AdminHome = () => {
     const {user} = useAuth();
 
     return (
         <div>
             <h2 className="text-5xl font-bold text-center">Welcome {user?.displayName}</h2>
+            <div>
+            <Lottie className='h-[70vh]' animationData={welcome} loop={true} />;
+            </div>
         </div>
     );
 };
--- src/pages/Dashboard/Student/StudentHome.jsx
+++ src/pages/Dashboard/Student/StudentHome.jsx
@@ -1,11 +1,16 @@
 import useAuth from "../../../Hooks/useAuth";
+import welcome from '../../../../public/welcome.json';
+import Lottie from "lottie-react";
 
 const StudentHome = () => {
     const {user} = useAuth();
     console.log(user);
     return (
-        <div>
-             <h2 className="text-5xl font-bold text-center">Welcome {user?.displayName}</h2>
+        <div className="mt-10">
+             <h2 className="text-5xl font-bold text-cyan-700 text-center">Welcome {user?.displayName}</h2>
+             <div>
+            <Lottie className='h-[70vh]' animationData={welcome} loop={true} />;
+            </div>
         </div>
     );
 };
