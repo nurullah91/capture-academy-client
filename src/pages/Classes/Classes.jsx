2bae67a93e4d14394107339c521a15eab0a29de5
nurullah91 <mdnurullah.ptk@gmail.com>
2023-10-07 00:30:19 +0600

    slider margin of small device added

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,4 +1,5 @@
 
+import Container from "../../Components/Container";
 import useClasses from "../../Hooks/useClasses";
 import ClassCard from "./ClassCard";
 
@@ -9,20 +10,23 @@ const Classes = () => {
   const approvedClasses = classes.filter((classItem) => classItem.status === 'approved');
 
   return (
-   <div className="overflow-hidden">
-     <div data-aos="zoom-in-up" className="my-24">
-      <h2 className="text-4xl my-5 font-bold text-center">Available Total Classes: {approvedClasses.length}</h2>
+    <div className="overflow-hidden">
+      <Container>
+        <div data-aos="zoom-in-up" className="my-24">
+          <h2 className="text-4xl my-5 font-bold text-center">All Classes</h2>
 
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
-        {/* show every single card */}
-        {
-          approvedClasses.map(singleClass => <ClassCard  key={singleClass._id} singleClass={singleClass}></ClassCard>)
-        }
-      </div>
+            {/* show every single card */}
+            {
+              approvedClasses.map(singleClass => <ClassCard key={singleClass._id} singleClass={singleClass}></ClassCard>)
+            }
+          </div>
+        </div>
+      </Container>
+
     </div>
-   </div>
   );
 };
 
--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -7,7 +7,7 @@ SwiperCore.use([Autoplay]);
 
 const Slider = () => {
     return (
-        <div className="">
+        <div className="mt-16 md:mt-0">
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
@@ -32,7 +32,7 @@ const Slider = () => {
                                 <h2 data-aos="fade-right" className='text-3xl lg:text-5xl font-semibold my-auto text-white w-3/4 md:w-1/2 mb-5 md:mb-2'>Capture Life&apos;s Beauty</h2>
                                 <p data-aos="fade-left" className='w-3/4 text-blue-50 mb-5 hidden md:block'>Join Capture Academy this summer to elevate your photography skills through expert guidance and hands-on learning.</p>
 
-                                <Link className='text-white border px-4 py-2 rounded' to="/classes">Get started</Link>
+                                <Link className='text-white border px-4 py-2 hover:bg-blue-500 rounded' to="/classes">Get started</Link>
                             </div>
 
                         </div>
@@ -50,7 +50,7 @@ const Slider = () => {
                                 <h2 data-aos="fade-right" className='text-3xl lg:text-5xl font-semibold my-auto text-white w-3/4 md:w-1/2 mb-5 md:mb-2'>Summer Camp for Young Shutterbugs</h2>
                                 <p data-aos="fade-left" className='w-3/4 text-blue-50 mb-5 hidden md:block'>Capture Academy offers a magical summer camp for young shutterbugs, combining creativity and adventure through photography.</p>
 
-                                <Link className='text-white border px-4 py-2 rounded' to="/classes">Get started</Link>
+                                <Link className='text-white border px-4 py-2 hover:bg-blue-500 rounded' to="/classes">Get started</Link>
                             </div>
 
                         </div>
@@ -66,7 +66,7 @@ const Slider = () => {
                                 <h2 data-aos="fade-right" className='text-3xl lg:text-5xl font-semibold my-auto text-white w-3/4 md:w-1/2 mb-5 md:mb-2'>Framing Memories, One Click at a Time</h2>
                                 <p data-aos="fade-left" className='w-3/4 text-blue-50 mb-5 hidden md:block'>Capture Academy is where art and technology converge, allowing you to frame memories one click at a time.</p>
 
-                                <Link className='text-white border px-4 py-2 rounded' to="/classes">Get started</Link>
+                                <Link className='text-white border px-4 py-2 hover:bg-blue-500 rounded' to="/classes">Get started</Link>
                             </div>
 
                         </div>
@@ -83,7 +83,7 @@ const Slider = () => {
                                 <h2 data-aos="fade-right" className='text-3xl lg:text-5xl font-semibold my-auto text-white w-3/4 md:w-1/2  mb-5 md:mb-2'>Summer Camp for Future Photographers</h2>
                                 <p data-aos="fade-left" className='w-3/4 text-blue-50 mb-5 hidden md:block'>Capture Academy is the place where young talents evolve into future photographers, nurturing skills and creativity for a bright future.</p>
 
-                                <Link className='text-white border px-4 py-2 rounded' to="/classes">Get started</Link>
+                                <Link className='text-white border px-4 py-2 hover:bg-blue-500 rounded' to="/classes">Get started</Link>
                             </div>
 
                         </div>
@@ -99,7 +99,7 @@ const Slider = () => {
                                 <h2 data-aos="fade-right" className='text-3xl lg:text-5xl font-semibold my-auto text-white w-3/4 md:w-1/2 mb-5 md:mb-2'>Discover Your Photographic Passion with Capture Academy</h2>
                                 <p data-aos="fade-left" className='w-3/4 text-blue-50 mb-5 hidden md:block'>Experience the joy of snapping, learning, and having fun at Capture Academy&apos;s photography-centered summer activities.</p>
 
-                                <Link className='text-white border px-4 py-2 rounded' to="/classes">Get started</Link>
+                                <Link className='text-white border px-4 py-2 hover:bg-blue-500 rounded' to="/classes">Get started</Link>
                             </div>
 
                         </div>
